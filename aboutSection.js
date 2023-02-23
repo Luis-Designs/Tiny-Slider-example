7dad58fe3889b4d925d6c6c033b5e5a1b7c55ea8
luis-designs <luislozano120xd@gmail.com>
2023-02-23 09:56:32 -0500

    :tada: add homeBannerInfo homeIWantToBetter

---
+++ aboutSection.js
@@ -0,0 +1,14 @@
+let sliderTwo = tns({
+    container: '.my-slider-two',
+    "slideBy": '1',
+    "speed": 400,
+    "nav": false,
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    responsive: {
+      480: {
+        items: 1,
+      },
+    },
+  });
